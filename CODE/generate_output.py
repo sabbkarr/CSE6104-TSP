3f7b5f5bed394c3f0a5ac3726da5347ed396f8ab
sabbkarr <167654171+sabbkarr@users.noreply.github.com>
2024-11-25 13:28:31 -0500

    Create generate_output.py

---
+++ CODE/generate_output.py
@@ -0,0 +1,29 @@
+import random
+city_list = ["Atlanta.tsp", "Champaign.tsp", "NYC.tsp", "SanFrancisco.tsp", "UMissouri.tsp",
+        "Berlin.tsp", "Cincinnati.tsp", "Philadelphia.tsp", "Toronto.tsp",
+        "Boston.tsp", "Denver.tsp", "Roanoke.tsp", "UKansasState.tsp"]
+
+
+selected_city = random.choice(city_list)
+print(f"Selected city: {selected_city.split('.')[0]}")
+points = read_tsp_file(f"/content/CSE6104-TSP/DATA/{selected_city}")
+
+# Brute Force
+time_limit = 60  
+best_route, best_cost = brute_force_tsp(points, time_limit)
+print("Best route found (bruteforce):", best_route)
+print("Best cost (bruteforce):", round(best_cost, 2))
+
+"""
+# Approximate
+best_route, best_cost = approximate_tsp(points)
+print("Best route found (approximate):", best_route)
+print("Best cost (approximate):", round(best_cost, 2))   
+"""
+
+"""
+# Local Search
+best_route, best_cost = local_search_tsp(points)
+print("Best route found (local search):", best_route)
+print("Best cost (local search):", round(best_cost, 2))   
+"""
